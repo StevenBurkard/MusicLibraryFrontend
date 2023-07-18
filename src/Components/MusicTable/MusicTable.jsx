2e3074bcf30aac846b8ec9da3f796e3f822828f6
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-07-18 14:57:57 -0500

    Added MusicTable and SearchBar components, wip: MusicTable to display the list of songs in the database

---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MusicTable(){
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                    <th>Likes</th>
+                    <th>Running Time</th>
+                </tr>
+            </thead>
+        </table>
+    );
+}
